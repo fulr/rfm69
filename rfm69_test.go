ee64ad9d61ee4e1f44ebf5651727153327364609
Friedl Ulrich <friedl.ulrich@gmail.com>
2015-02-02 22:07:19 +0100

    Added Test

--- rfm69_test.go
+++ rfm69_test.go
@@ -1,14 +1,14 @@
-package main
+package rfm69
 
 import (
 	"fmt"
+	"testing"
 
-	"github.com/fulr/rfm69"
 	"github.com/kidoman/embd"
 	_ "github.com/kidoman/embd/host/all"
 )
 
-func main() {
+func TestRfm69(t *testing.T) {
 	fmt.Print("Test")
 
 	if err := embd.InitSPI(); err != nil {
@@ -19,8 +19,9 @@ func main() {
 	spiBus := embd.NewSPIBus(embd.SPIMode0, 0, 4000000, 8, 0)
 	defer spiBus.Close()
 
-	rfm, err := rfm69.NewDevice(spiBus, 1, 10, true)
+	rfm, err := NewDevice(spiBus, 1, 10, true)
 	if err != nil {
 		panic(err)
 	}
+	print(rfm)
 }
